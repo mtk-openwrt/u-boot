d8e41d199cb91c56ba693fc81922fefe03c2fa3b
Weijie Gao <weijie.gao@mediatek.com>
2022-07-25 17:21:06 +0800

    boot: add support for MediaTek Anti-rollback implementation
    Add MediaTek Anti-rollback implementation for boot
    
    Signed-off-by: Weijie Gao <weijie.gao@mediatek.com>

--- boot/image-fit.c
+++ boot/image-fit.c
@@ -39,6 +39,15 @@ DECLARE_GLOBAL_DATA_PTR;
 #include <upl.h>
 #include <u-boot/crc.h>
 
+#ifndef USE_HOSTCC
+__weak
+#endif
+int mtk_ar_verify_fw_ar_ver(const void *fit, int conf_noffset,
+			    uint32_t *fw_ar_ver)
+{
+	return 0;
+}
+
 /*****************************************************************************/
 /* New uImage format routines */
 /*****************************************************************************/
@@ -1876,6 +1885,12 @@ int fit_conf_get_node(const void *fit, const char *conf_uname)
 	return noffset;
 }
 
+int fit_conf_get_fw_ar_ver(const void *fit, int conf_noffset, ulong *fw_ar_ver)
+{
+	return fit_image_get_address(fit, conf_noffset,
+				     FIT_FW_AR_VER_PROP, fw_ar_ver);
+}
+
 int fit_conf_get_prop_node_count(const void *fit, int noffset,
 		const char *prop_name)
 {
@@ -2131,6 +2146,10 @@ int fit_image_load(struct bootm_headers *images, ulong addr,
 				return -EACCES;
 			}
 			puts("OK\n");
+			if (mtk_ar_verify_fw_ar_ver(fit, cfg_noffset,
+						    &images->fw_ar_ver)) {
+				return -EACCES;
+			}
 		}
 
 		bootstage_mark(BOOTSTAGE_ID_FIT_CONFIG);
