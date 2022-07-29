e4f22245ddc3c89445575c4046bf359befffb83a
Weijie Gao <weijie.gao@mediatek.com>
2022-07-29 15:36:05 +0800

    board: mt7986: add bootmenu support
    This adds bootmenu and related features for MT7986 boards
    
    Signed-off-by: Weijie Gao <weijie.gao@mediatek.com>

--- include/configs/mt7986.h
+++ include/configs/mt7986.h
@@ -9,4 +9,14 @@
 #ifndef __MT7986_H
 #define __MT7986_H
 
+/* Extra environment variables */
+#ifdef CONFIG_MTK_DEFAULT_FIT_BOOT_CONF
+#define FIT_BOOT_CONF_ENV	"bootconf=" CONFIG_MTK_DEFAULT_FIT_BOOT_CONF "\0"
+#else
+#define FIT_BOOT_CONF_ENV
+#endif
+
+#define CFG_EXTRA_ENV_SETTINGS	\
+	FIT_BOOT_CONF_ENV
+
 #endif
