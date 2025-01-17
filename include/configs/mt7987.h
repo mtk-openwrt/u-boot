70e5cec9ab6536007a9227a4264402fdcba86ca5
Weijie Gao <weijie.gao@mediatek.com>
2025-01-17 09:12:19 +0800

    board: mt7987: add bootmenu support
    This adds bootmenu and related features for MT7987 boards
    
    Signed-off-by: Weijie Gao <weijie.gao@mediatek.com>

--- include/configs/mt7987.h
+++ include/configs/mt7987.h
@@ -11,4 +11,14 @@
 
 #define CFG_MAX_MEM_MAPPED		0xC0000000
 
+/* Extra environment variables */
+#ifdef CONFIG_MTK_DEFAULT_FIT_BOOT_CONF
+#define FIT_BOOT_CONF_ENV	"bootconf=" CONFIG_MTK_DEFAULT_FIT_BOOT_CONF "\0"
+#else
+#define FIT_BOOT_CONF_ENV
+#endif
+
+#define CFG_EXTRA_ENV_SETTINGS	\
+	FIT_BOOT_CONF_ENV
+
 #endif
